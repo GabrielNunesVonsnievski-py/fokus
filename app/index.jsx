caba98e46fe480ad64f7cf575857aab16007bfc8
GabrielNunesVonsnievski-py <gabriel_nunes2017@outlook.com>
2025-11-13 11:28:50 -0300

    Alterações 13/11/25

---
+++ app/index.jsx
@@ -0,0 +1,73 @@
+import { Image, StyleSheet, Text, View, Pressable } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Image source={require('./andando.png')}/>
+      <View style={styles.actions}>
+        <Text style={styles.timer}>
+          25:00
+        </Text>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>
+            Começar
+          </Text>
+        </Pressable>
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>
+          Projeto desenvolvido sem fins comerciais.
+        </Text>
+        <Text style={styles.footerText}>
+          Desenvolvido por Gabriel Nunes Vonsnievki.
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: '#021123',
+    gap: 40
+  },
+  actions:{
+    paddingVertical: 24,
+    paddingHorizontal: 24,
+    backgroundColor: '#14448080',
+    width: '80%',
+    borderRadius: 32,
+    borderWidth: 2,
+    borderColor: '#144480',
+    gap: 32
+  },
+  timer:{
+    fontSize: 54,
+    color: '#FFF',
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  button:{
+    backgroundColor: '#B872FF',
+    borderRadius: 32,
+    padding: 8
+  },
+  buttonText:{
+    textAlign: 'center',
+    color: '#021132',
+    fontSize: 18
+  },
+  footer:{
+    width: '80%'
+  },
+  footerText:{
+    textAlign: 'center',
+    color: '#98A0A8',
+    fontSize: 12.5
+  }
+})
