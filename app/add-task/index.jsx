c948d08d2466c8abb84b45a276d42193edcafd53
GabrielNunesVonsnievski-py <gabriel_nunes2017@outlook.com>
2025-11-17 17:20:29 -0300

    Lista de tarefas dinamica e scroll view

--- app/add-task/index.jsx
+++ app/add-task/index.jsx
@@ -1,12 +1,94 @@
-import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Text, View, TextInput, Pressable, StyleSheet, KeyboardAvoidingView, Platform, Keyboard } from "react-native";
+import { IconSave } from "../../components/Icons";
+import useTaskContext from "../../components/context/useTaskContext";
+import { useState } from "react";
+import { router } from "expo-router";
 
-export default function AddText () {
+export default function AddTask() {
+
+    const [description, setDescription] = useState()
+
+    const { addTask } = useTaskContext()
+
+    const submitTask = () => {
+        if(!description){
+            return
+        }
+        addTask(description)
+        setDescription('')
+        router.navigate('/tasks')
+    }
+
     return (
-    <SafeAreaView>
-        <Text>
-            Adicionar uma Tarefa
-        </Text>
-    </SafeAreaView>
+        <KeyboardAvoidingView
+            style={styles.container}
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        >
+            <Pressable onPress={Keyboard.dismiss}>
+                <View style={styles.inner}>
+                    <Text style={styles.text}>
+                        Adicionar uma Tarefa
+                    </Text>
+                    <Text style={styles.label}>
+                        Em que você está trabalhando?
+                    </Text>
+                    <TextInput
+                        style={styles.input}
+                        numberOfLines={10}
+                        multiline={true}
+                        value={description}
+                        onChangeText={setDescription}
+                    />
+                    <View style={styles.actions}>
+                        <Pressable style={styles.button} onPress={submitTask}>
+                            <IconSave />
+                            <Text>
+                                Salvar
+                            </Text>
+                        </Pressable>
+                    </View>
+                </View>
+            </Pressable>
+        </KeyboardAvoidingView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#021123',
+        gap: 16,
+        alignItems: 'center'
+    },
+    text: {
+        color: '#FFF',
+        textAlign: 'center',
+        fontSize: 26
+    },
+    inner: {
+        backgroundColor: '#98A0A8',
+        width: '90%',
+        borderRadius: 8,
+        padding: 16,
+        gap: 32
+    },
+    label: {
+        fontWeight: 600,
+        fontSize: 18
+    },
+    input: {
+        backgroundColor: '#FFF',
+        padding: 16,
+        borderRadius: 8,
+        height: 100
+    },
+    button: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 4
+    },
+    actions: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end'
+    }
+})
---
+++ components/context/TaskProvider.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { createContext } from "react";
+
+export const TaskContext = createContext()
+
+export function TasksProvider({ children }) {
+
+    const [tasks, setTasks] = useState([])
+
+    const addTask = (description) => {
+        console.log('tarefa vai ser adicionada')
+        setTasks(oldState => {
+            return[
+                ...oldState,
+                {
+                    description,
+                    id: oldState.length + 1
+                }
+            ]
+        })
+    }
+
+    const toggleTaskCompleted = () => {
+        setTasks(oldState => {
+            return oldState.map(t => {
+                if(t.id == id){
+                    t.completed = !t.completed
+                }
+                return t
+            })
+        })
+    }
+
+    const deleteTask = () => {
+        setTasks(oldState => {
+            return oldState.filter(t => t.id != id)
+        })
+    }
+
+
+
+    return (
+        <TaskContext.Provider value={{
+            tasks,
+            addTask,
+            toggleTaskCompleted,
+            deleteTask
+        }}>
+            { children }
+        </TaskContext.Provider>
+    )
+}
